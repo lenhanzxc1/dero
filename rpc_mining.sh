d6d50d78910240786f79af8ea71e66e4693c9060
Lê Ngọc Nhân <128038640+lenhanzxc1@users.noreply.github.com>
2024-06-27 13:19:28 +0700

    rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+echo "Vui Lòng Chỉnh Địa Chỉ Ví Hoặc Pool Đào"
 while :; do
-    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqeqpmjy49sqhq2eu -r community-pools.mysrv.cloud:10300 -m 2 -p rpc;
+    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqeqpmjy49sqhq2eu -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
